708b804fea1c95174a8757a4e7cb0166b7d15b4e
Carol Willing <carolcode@willingconsulting.com>
2016-10-24 13:57:38 -0700

    Fix docstrings and formatting in services/auth

--- jupyterhub/services/auth.py
+++ jupyterhub/services/auth.py
@@ -1,10 +1,12 @@
 """Authenticating services with JupyterHub
 
-Cookies are sent to the Hub for verification, replying with a JSON model describing the authenticated user.
+Cookies are sent to the Hub for verification, replying with a JSON model
+describing the authenticated user.
 
 HubAuth can be used in any application, even outside tornado.
 
-HubAuthenticated is a mixin class for tornado handlers that should authenticate with the Hub.
+HubAuthenticated is a mixin class for tornado handlers that should
+authenticate with the Hub.
 """
 
 import os
@@ -47,7 +49,7 @@ class _ExpiringDict(dict):
     def _check_age(self, key):
         """Check timestamp for a key"""
         if key not in self.values:
-            # not registered, nothing to do
+            # key not registered, nothing to do
             return
         now = time.monotonic()
         timestamp = self.timestamps[key]
@@ -92,44 +94,53 @@ class HubAuth(Configurable):
 
     - api_url: the base URL of the Hub's internal API
     - cookie_cache_max_age: the number of seconds responses
-      from the Hub should be cached.
+                            from the Hub should be cached.
     """
 
-    # where is the hub
-    api_url = Unicode(os.environ.get('JUPYTERHUB_API_URL') or 'http://127.0.0.1:8081/hub/api',
-        help="""The base API URL of the Hub.
+    # hub location
+    api_url = Unicode(os.environ.get('JUPYTERHUB_API_URL') or
+                      'http://127.0.0.1:8081/hub/api',
+                      help="""The base API URL of the Hub.
 
-        Typically http://hub-ip:hub-port/hub/api
-        """
-    ).tag(config=True)
+                      Typically http://hub-ip:hub-port/hub/api
+                      """
+                     ).tag(config=True)
 
     login_url = Unicode('/hub/login',
-        help="""The login URL of the Hub
+                        help="""The login URL of the Hub
         
-        Typically /hub/login
-        """
-    ).tag(config=True)
+                             Typically /hub/login
+                             """
+                       ).tag(config=True)
 
     api_token = Unicode(os.environ.get('JUPYTERHUB_API_TOKEN', ''),
-        help="""API key for accessing Hub API.
+                        help="""API key for accessing Hub API.
 
-        Generate with `jupyterhub token [username]` or add to JupyterHub.services config.
-        """
-    ).tag(config=True)
+                             Generate with `jupyterhub token [username]` or
+                             add to JupyterHub.services config.
+                             """
+                       ).tag(config=True)
 
     cookie_name = Unicode('jupyterhub-services',
-        help="""The name of the cookie I should be looking for"""
-    ).tag(config=True)
+                          help="""The name of the cookie I should be using"""
+                         ).tag(config=True)
+
     cookie_cache_max_age = Integer(300,
-        help="""The maximum time (in seconds) to cache the Hub's response for cookie authentication.
+                                   help="""The maximum time (in seconds) to
+                                        cache the Hub's response for cookie
+                                        authentication.
 
-        A larger value reduces load on the Hub and occasional response lag.
-        A smaller value reduces propagation time of changes on the Hub (rare).
+                                        A larger value reduces load on the Hub
+                                        and occasional response lag.
+                                        A smaller value reduces propagation
+                                        time of changes on the Hub (rare).
 
-        Default: 300 (five minutes)
-        """
-    ).tag(config=True)
+                                        Default: 300 (five minutes)
+                                        """
+                                  ).tag(config=True)
+
     cookie_cache = Instance(_ExpiringDict, allow_none=False)
+
     @default('cookie_cache')
     def _cookie_cache(self):
         return _ExpiringDict(self.cookie_cache_max_age)
@@ -138,11 +149,14 @@ class HubAuth(Configurable):
         """Ask the Hub to identify the user for a given cookie.
 
         Args:
-            encrypted_cookie (str): the cookie value (not decrypted, the Hub will do that)
-            use_cache (bool): Specify use_cache=False to skip cached cookie values (default: True)
+            encrypted_cookie (str): the cookie value (not decrypted, the Hub
+                                    will do that)
+            use_cache (bool): Specify use_cache=False to skip cached cookie
+                              values (default: True)
 
         Returns:
-            user_model (dict): The user model, if a user is identified, None if authentication fails.
+            user_model (dict): The user model, if a user is identified,
+                               None if authentication fails.
 
             The 'name' field contains the user's name.
         """
@@ -156,29 +170,37 @@ class HubAuth(Configurable):
                               "authorizations/cookie",
                               self.cookie_name,
                               quote(encrypted_cookie, safe=''),
-                ),
-                headers = {
-                    'Authorization' : 'token %s' % self.api_token,
+                             ),
+                headers={
+                    'Authorization': 'token %s' % self.api_token,
                 },
             )
         except requests.ConnectionError:
-            msg = "Failed to connect to Hub API at %r." % self.api_url
-            msg += "  Is the Hub accessible at this URL (from host: %s)?" % socket.gethostname()
+            msg = "Failed to connect to Hub API at %r.", self.api_url
+            msg += ("  Is the Hub accessible at this URL (from host: %s)?",
+                    socket.gethostname())
             if '127.0.0.1' in self.api_url:
-                msg += "  Make sure to set c.JupyterHub.hub_ip to an IP accessible to" + \
-                       " single-user servers if the servers are not on the same host as the Hub."
+                msg += ("  Make sure to set c.JupyterHub.hub_ip to an IP"
+                        "accessible to single-user servers if the servers are"
+                        " not on the same host as the Hub.")
             raise HTTPError(500, msg)
 
         if r.status_code == 404:
             data = None
         elif r.status_code == 403:
-            app_log.error("I don't have permission to verify cookies, my auth token may have expired: [%i] %s", r.status_code, r.reason)
-            raise HTTPError(500, "Permission failure checking authorization, I may need a new token")
+            app_log.error("I don't have permission to verify cookies, my auth"
+                          "token may have expired: [%i] %s",
+                          r.status_code, r.reason)
+            raise HTTPError(500, "Permission failure checking authorization,"
+                                 "I may need a new token")
         elif r.status_code >= 500:
-            app_log.error("Upstream failure verifying auth token: [%i] %s", r.status_code, r.reason)
-            raise HTTPError(502, "Failed to check authorization (upstream problem)")
+            app_log.error("Upstream failure verifying auth token: [%i] %s",
+                          r.status_code, r.reason)
+            raise HTTPError(502, "Failed to check authorization (upstream "
+                                 "problem)")
         elif r.status_code >= 400:
-            app_log.warn("Failed to check authorization: [%i] %s", r.status_code, r.reason)
+            app_log.warning("Failed to check authorization: [%i] %s",
+                            r.status_code, r.reason)
             raise HTTPError(500, "Failed to check authorization")
         else:
             data = r.json()
@@ -194,9 +216,9 @@ class HubAuth(Configurable):
             handler (tornado.web.RequestHandler): the current request handler
 
         Returns:
-            user_model (dict): The user model, if a user is identified, None if authentication fails.
-
-            The 'name' field contains the user's name.
+            user_model (dict): The user model, if a user is identified,
+                               None if authentication fails.
+                               The 'name' field contains the user's name.
         """
 
         # only allow this to be called once per handler
@@ -219,11 +241,11 @@ class HubAuth(Configurable):
 class HubAuthenticated(object):
     """Mixin for tornado handlers that are authenticated with JupyterHub
 
-    A handler that mixes this in must have the following attributes/properties:
+    A handler using this mixin must have the following attributes/properties:
 
     - .hub_auth: A HubAuth instance
     - .hub_users: A set of usernames to allow.
-      If left unspecified or None, any Hub user will be allowed.
+                  If left unspecified or None, any Hub user will be allowed.
 
     Examples::
 
@@ -236,17 +258,20 @@ class HubAuthenticated(object):
             @web.authenticated
             def get(self):
                 ...
-
     """
-    hub_users = None # set of allowed users
-    
-    # self.hub_auth must be a HubAuth instance.
+    hub_users = None  # set of allowed users
+    _hub_auth = None
+
+    @property
+    def hub_auth(self):
+        """Set a HubAuth instance.
+
+        # self.hub_auth must be a HubAuth instance.
     # If nothing specified, use default config,
     # which will be configured with defaults
     # based on JupyterHub environment variables for services.
-    _hub_auth = None
-    @property
-    def hub_auth(self):
+
+        """
         if self._hub_auth is None:
             self._hub_auth = HubAuth()
         return self._hub_auth
@@ -260,12 +285,14 @@ class HubAuthenticated(object):
 
         Returns the input if the user should be allowed, None otherwise.
 
-        Override if you want to check anything other than the username's presence in hub_users list.
+        Override if you want to check anything other than the username's
+        presence in hub_users list.
 
         Args:
             user_model (dict): the user model returned from :class:`HubAuth`
         Returns:
-            user_model (dict): The user model if the user should be allowed, None otherwise.
+            user_model (dict): The user model if the user should be allowed,
+                               None otherwise.
         """
         if self.hub_users is None:
             # no users specified, allow any authenticated Hub user
@@ -274,17 +301,17 @@ class HubAuthenticated(object):
         if name in self.hub_users:
             return user_model
         else:
-            app_log.warn("Not allowing Hub user %s" % name)
+            app_log.warning("Not allowing Hub user %s", name)
             return None
 
     def get_current_user(self):
         """Tornado's authentication method
 
         Returns:
-            user_model (dict): The user model, if a user is identified, None if authentication fails.
+            user_model (dict): The user model, if a user is identified,
+                               None if authentication fails.
         """
         user_model = self.hub_auth.get_user(self)
         if not user_model:
             return
         return self.check_hub_user(user_model)
-
