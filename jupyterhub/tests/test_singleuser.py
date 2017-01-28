f24c8f26faa6051648a93f2b9c6c28efb45393f1
Carol Willing <carolcode@willingconsulting.com>
2017-01-27 22:10:04 -0800

    Parameterize single user CLI tests

--- jupyterhub/tests/test_singleuser.py
+++ jupyterhub/tests/test_singleuser.py
@@ -3,6 +3,7 @@
 from subprocess import check_output
 import sys
 
+import pytest
 import requests
 
 import jupyterhub
@@ -10,41 +11,46 @@ from .mocking import StubSingleUserSpawner, public_url
 from ..utils import url_path_join
 
 
-def test_singleuser_auth(app, io_loop):
+@pytest.mark.parametrize("test_username", ['nandy'])
+def test_singleuser_auth(app, io_loop, test_username):
+    """Test user authentication to single user server"""
     # use StubSingleUserSpawner to launch a single-user app in a thread
     app.spawner_class = StubSingleUserSpawner
     app.tornado_settings['spawner_class'] = StubSingleUserSpawner
-    
+
     # login, start the server
-    cookies = app.login_user('nandy')
-    user = app.users['nandy']
+    cookies = app.login_user(test_username)
+    user = app.users[test_username]
     if not user.running:
         io_loop.run_sync(user.spawn)
     url = public_url(app, user)
-    
+
     # no cookies, redirects to login page
     r = requests.get(url)
     r.raise_for_status()
     assert '/hub/login' in r.url
-    
+
     # with cookies, login successful
     r = requests.get(url, cookies=cookies)
     r.raise_for_status()
-    assert r.url.rstrip('/').endswith('/user/nandy/tree')
     assert r.status_code == 200
-    
+    assert r.url.rstrip('/').endswith(
+        url_path_join('/user/', test_username, '/tree')
+    )
+
     # logout
     r = requests.get(url_path_join(url, 'logout'), cookies=cookies)
     assert len(r.cookies) == 0
 
 
-def test_disable_user_config(app, io_loop):
+@pytest.mark.parametrize("test_username", ['nandy'])
+def test_disable_user_config(app, io_loop, test_username):
     # use StubSingleUserSpawner to launch a single-user app in a thread
     app.spawner_class = StubSingleUserSpawner
     app.tornado_settings['spawner_class'] = StubSingleUserSpawner
     # login, start the server
-    cookies = app.login_user('nandy')
-    user = app.users['nandy']
+    cookies = app.login_user(test_username)
+    user = app.users[test_username]
     # stop spawner, if running:
     if user.running:
         print("stopping")
@@ -53,22 +59,29 @@ def test_disable_user_config(app, io_loop):
     user.spawner.debug = True
     user.spawner.disable_user_config = True
     io_loop.run_sync(user.spawn)
-    io_loop.run_sync(lambda : app.proxy.add_user(user))
-    
+    io_loop.run_sync(lambda: app.proxy.add_user(user))
+
     url = public_url(app, user)
-    
+
     # with cookies, login successful
     r = requests.get(url, cookies=cookies)
     r.raise_for_status()
-    assert r.url.rstrip('/').endswith('/user/nandy/tree')
     assert r.status_code == 200
+    assert r.url.rstrip('/').endswith(
+        url_path_join('/user/', test_username, '/tree')
+    )
 
 
-def test_help_output():
-    out = check_output([sys.executable, '-m', 'jupyterhub.singleuser', '--help-all']).decode('utf8', 'replace')
+@pytest.mark.parametrize("option", ['--help', '-h'])
+def test_help_output(option):
+    command = [sys.executable, '-m', 'jupyterhub.singleuser', option]
+
+    out = check_output(command).decode('utf8', 'replace')
     assert 'JupyterHub' in out
 
-def test_version():
-    out = check_output([sys.executable, '-m', 'jupyterhub.singleuser', '--version']).decode('utf8', 'replace')
-    assert jupyterhub.__version__ in out
 
+@pytest.mark.parametrize("option", ['--version'])
+def test_version(option):
+    command = [sys.executable, '-m', 'jupyterhub.singleuser', option]
+    out = check_output(command).decode('utf8', 'replace')
+    assert jupyterhub.__version__ in out
