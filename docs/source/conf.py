1b233fa3373300a72f1c2e5659f60198d5e22488
Carol Willing <carolcode@willingconsulting.com>
2018-09-17 22:02:45 -0700

    unpin recommonmark

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,6 @@ import os
 import shlex
 
 import recommonmark
-from recommonmark.transform import AutoStructify
 
 # Set paths
 sys.path.insert(0, os.path.abspath('.'))
@@ -200,10 +199,3 @@ else:
     extensions.append("sphinxcontrib.spelling")
 
 spelling_word_list_filename = 'spelling_wordlist.txt'
-
-# app setup hook
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
